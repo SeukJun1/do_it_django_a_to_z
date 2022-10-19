8856febd7c1faaae30dc6c493496f313e2c1edd3
SeukJun Kim <lastluna70@naver.com>
2022-10-19 16:46:16 +0900

    대문 페이지와 자기소개 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path('<int:pk>/', views.single_post_page),
-    path("", views.index),
+    path('', views.index),
+
 ]
